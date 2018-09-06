0d4cc2d9a3dec0a15445e0af0a4d7bef33c76922
Eric <ericmiller@umn.edu>
2018-09-05 22:06:37 -0500

    added aria labels in a few spots

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -177,7 +177,8 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View Details'
+  more.setAttribute('aria-label',`View Details of ${restaurant.name}`);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
